278dddeaf51a6b3626e94156d742ddb61e4db382
Kunuk Nykjær <kunukn@gmail.com>
2017-06-24 21:37:22 +0200

    add code

--- react/components-communicate-through-reflux/src/stores/Shop/ShopActions.js
+++ react/components-communicate-through-reflux/src/stores/Shop/ShopActions.js
@@ -3,3 +3,24 @@ import {createActions} from 'reflux';
 const ShopActions = createActions(['init', 'addToInventory', 'removeFromInventory']);
 
 export default ShopActions;
+
+
+
+
+/*
+const ShopActions = createActions({
+    init: {},
+    addToInventory: {},
+    removeFromInventory: {},
+    load: {
+        asyncResult: true,
+        children: ['completed', 'failed']
+    }
+});
+ShopActions.load.listen(function () {
+        fetchShopData({
+            completed: this.completed, 
+            failed: this.failed
+        });
+    });
+*/
