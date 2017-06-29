911f39fb0910385fe1f0fd0943a71f1b75ef6dd9
Kunuk Nykjær <kunukn@gmail.com>
2017-06-30 00:00:31 +0200

    fun with animations

--- react/components-communicate-through-reflux/src/components/Products/Products.js
+++ react/components-communicate-through-reflux/src/components/Products/Products.js
@@ -6,7 +6,7 @@ import _ from 'lodash';
 //import {log} from '../../utils';
 // store
 import ShopStore from '../../stores/Shop/ShopStore';
-import ShopActions from '../../stores/Shop/ShopActions';
+import {addToInventory} from '../../stores/Shop/ShopActions';
 import TrackerStore from '../../stores/Tracker/TrackerStore';
 import {track} from '../../stores/Tracker/TrackerActions';
 // components
@@ -40,8 +40,8 @@ class Products extends Reflux.Component {
                 {products && products.length > 0 && <ul className='products__list'>
                     {products.map((product, index) => {
                         const onAdd = (domProductItemIcon) => {
-                            animateProductItem.bind(this)(domProductItemIcon);
-                            ShopActions.addToInventory(product);
+                            animateProductItem.call(this,domProductItemIcon);
+                            addToInventory(product);
                             track({action: 'add', productId: product.id});
                         }
                         const stockItem = _.find(stock, {id: product.id});
@@ -66,30 +66,49 @@ class Products extends Reflux.Component {
 
 function animateProductItem(domProductItem) {
 
+    /*
+        imperative code which could be cleaned up 
+        into function calls
+    */
+
+    // Create icon mover, which shakes
     let parent = domProductItem.parentElement;
     let iconAnimation = domProductItem.cloneNode(true);
     iconAnimation.classList.add('product-item__icon--animate');
-    parent.appendChild(iconAnimation);
+    if(Math.random() > .5){
+        iconAnimation.style.animationDirection = 'reverse';
+    }
 
-    // getBoundingClientRect causes repaint for browser, use sparringly
-    let rect = domProductItem.getBoundingClientRect(); // can  reflow
-    let dest = this.state.basketLocation;
-    let x = ~~(dest.left + 1*dest.width / 2 - rect.left - 0*rect.width / 2);
-    let y = ~~(dest.top + 1*dest.height / 2 - rect.top - 0*rect.height / 2);
-    let currentTransform = getComputedStyle(iconAnimation)
-        .transform
-        .replace('none', '');
-    iconAnimation.style.transform = `${currentTransform} translateX(${x}px) translateY(${y}px) translateZ(0)`;
-    iconAnimation.style.opacity = '0';
+    // Create icon wrap which moves to basket
+    let iconAnimationWrap = document.createElement('div');
+    iconAnimationWrap.className = 'product-item__icon--animate-wrap';
+    iconAnimationWrap.appendChild(iconAnimation);
+    
+    // Add to DOM
+    parent.appendChild(iconAnimationWrap);
 
-    ((iconAnimation) => {
-        // DOM cleanup
-        setTimeout(() => {
-            if(iconAnimation){
-                parent.removeChild(iconAnimation);
-            }
-        }, 2000);
-    })(iconAnimation)
+    // Animate to basket by using transition and transform
+    window.requestAnimationFrame(()=>{
+        // getBoundingClientRect causes repaint for browser, use sparringly
+        let rect = domProductItem.getBoundingClientRect(); // repaint
+        let dest = this.state.basketLocation;
+        let x = ~~(dest.left + 1*dest.width / 2 - rect.left - 0*rect.width / 2);
+        let y = ~~(dest.top + 1*dest.height / 2 - rect.top - 0*rect.height / 2);
+        let currentTransform = getComputedStyle(iconAnimationWrap)
+            .transform
+            .replace('none', '');
+        iconAnimationWrap.style.transform = `${currentTransform} translateX(${x}px) translateY(${y}px) translateZ(0)`;
+        iconAnimationWrap.style.opacity = '0';
+
+        ((el) => {
+            // DOM cleanup
+            setTimeout(() => {
+                if(el){
+                    parent.removeChild(el);
+                }
+            }, 10000);
+        })(iconAnimationWrap);
+    });
 }
 
 export default Products;
