13d1f6f12aac8bda4a81685dff2eda4e2e0423e9
Kunuk Nykjær <kny@tdc.dk>
2017-12-12 15:51:34 +0100

    new

---
+++ react/text-box/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './app.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 'type' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+          <div>{this.state.value}</div>
+        </form>
+      </div>
+    );
+  }
+}
