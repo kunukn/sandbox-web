93421127b30cf5809c9053bdaa03aaf1791dc665
Kunuk Nykjær <kny@tdc.dk>
2017-12-21 17:00:57 +0100

    use Promise.all

--- react/text-box/src/services/calculatorServices.js
+++ react/text-box/src/services/calculatorServices.js
@@ -9,8 +9,9 @@ import { createSuccessPromise, createFailurePromise } from 'utilities/promises';
 export async function calculateService({ value }) {
   const person = await getPerson({ value });
   if (person && !_.isNil(person.val1) && !_.isNil(person.val2)) {
-    const facility = await getFacility({ value: person.val1 });
-    const exposure = await getExposure({ value: person.val2 });
+    const facilityPromise =  getFacility({ value: person.val1 });
+    const exposurePromise =  getExposure({ value: person.val2 });
+    const [facility, exposure] = await Promise.all([facilityPromise, exposurePromise])
     const result = parseInt(facility.val3, 10) * parseInt(exposure.val5, 10);
     return createSuccessPromise({ result });
   }
