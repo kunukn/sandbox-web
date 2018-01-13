4c8506639da7c2c2fbd5ac1453a3f7568f08e575
Kunuk Nykjær <kunukn@gmail.com>
2018-01-13 13:31:02 +0100

    make redux extension work

--- react/text-box/src/store/configureStore.js
+++ react/text-box/src/store/configureStore.js
@@ -9,6 +9,7 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),    
+    applyMiddleware(thunk, reduxImmutableStateInvariant()),
   );
 }
