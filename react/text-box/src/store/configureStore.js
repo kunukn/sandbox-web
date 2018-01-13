4601bc5e25aba10d529e3eabb9cd0a87c3d4c72c
Kunuk Nykjær <kunukn@gmail.com>
2018-01-13 16:06:02 +0100

    make redux extension work again

--- react/text-box/src/store/configureStore.js
+++ react/text-box/src/store/configureStore.js
@@ -1,15 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from 'reducers/';
 import thunk from 'redux-thunk';
 
 /* Redux middleware that show error when you try to mutate your state either inside a dispatch or between dispatches */
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk, reduxImmutableStateInvariant()),
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())),
   );
 }
